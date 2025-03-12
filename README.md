2e468a832b4d85831e0ba73bec68a36dab1d0d66
Edgar Lorenz G. Castro <elgc@Edgars-iMac.local>
2025-03-12 09:48:02 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Mb-Ingredients
+
+This is a React application designed to monitor price changes in our product ingredients.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>MB-Ingredients</div>
+  )
+}
+
+export default App
